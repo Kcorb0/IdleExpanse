75fa324ea0fa5c7fdb29d5c86034cdc4b390f4fc
Kcorb0 <68121651+Kcorb0@users.noreply.github.com>
2022-05-11 23:57:08 +0100

    Created some classes to test logic

---
+++ sandbox/main.py
@@ -0,0 +1,7 @@
+# Universe (Local Void) -> Galaxy -> Star System -> Planets
+
+
+class Galaxy:
+    def __init__(self, name, num_stars):
+        self.name = name
+        self.num_stars = num_stars
